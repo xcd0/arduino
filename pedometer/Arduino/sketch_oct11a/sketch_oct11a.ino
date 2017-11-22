adc81b830bbb97c8e5b26168e5eee26e9233c933
sho.hiramatsu <c5908@st.cs.kumamoto-u.ac.jp>
2017-11-22 10:59:43 +0900

    add files

---
+++ pedometer/Arduino/sketch_oct11a/sketch_oct11a.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int ans;
+  ans = analogRead(0);
+  Serial.println(ans);
+  delay(100);
+}
---
+++ pedometer/Arduino/sketch_oct18a/sketch_oct18a.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int ans;
+
+  ans = analogRead(0);
+  if(ans >= 700){
+    //光が値より暗くなったなら処理
+    digitalWrite(6,HIGH);
+  }
+  else{
+    digitalWrite(6,LOW);
+  }
+  
+
+}
