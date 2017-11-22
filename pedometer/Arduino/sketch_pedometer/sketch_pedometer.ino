4cb7c6826b1af637460b10c2079acee22c99c732
yuuki <nekokan53699@gmail.com>
2017-11-22 11:23:01 +0900

    add souse code

---
+++ pedometer/Arduino/sketch_pedometer/sketch_pedometer.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+     pinMode(13,OUTPUT) ;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+     int ans ;
+
+     ans = analogRead(0) ;
+     if (ans >= 700) {
+          digitalWrite(13, HIGH) ;
+      } else {
+          digitalWrite(13, LOW) ;
+      }
+}
