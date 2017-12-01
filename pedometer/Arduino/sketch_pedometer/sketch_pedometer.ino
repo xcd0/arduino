52175b39279719abb614ffb74961f70d484a906c
xcd0 <xcd0@xcd0.com>
2017-12-01 17:54:31 +0900

    aaa

--- pedometer/Arduino/sketch_pedometer/sketch_pedometer.ino
+++ pedometer/Arduino/sketch_pedometer/sketch_pedometer.ino
@@ -1,17 +1,21 @@
 void setup() {
   // put your setup code here, to run once:
-     pinMode(13,OUTPUT) ;
+     Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-     int ans ;
+     int i, a;
+     long x = 0, y = 0, z = 0;
 
-  // unko
+     for( i = 0; i < 100; i++ ){
+      x += analogRead( 3 );
+     }
      ans = analogRead(0) ;
      if (ans >= 700) {
           digitalWrite(13, HIGH) ;
       } else {
           digitalWrite(13, LOW) ;
       }
+      Serial.println(ans);
 }
